57597d3c4fe79db630881b9ba1bf02befcba49ab
Nikole Chetty <nchetty@andrew.cmu.edu>
2022-07-14 11:40:49 -0400

    added pause between combinations

--- clock_gui/analog_clock_with_ardunio_randomized.py
+++ clock_gui/analog_clock_with_ardunio_randomized.py
@@ -13,9 +13,9 @@ ctypes.windll.shcore.SetProcessDpiAwareness(1)
 arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
 
 ANGLES = [10, 20] # small, medium, and large targets 
-SPEED = [5, 10] # slow, medium, and fast cursor speeds
+SPEED = [10, 15] # slow, medium, and fast cursor speeds
 
-START_ANGLE = 90         # 0 deg is north, degrees go clockwise for positive
+START_ANGLE = 180         # 0 deg is north, degrees go clockwise for positive
 # EXTENT_ANGLE = 60
 # ACTIVE_TIME = 20000 # 20 seconds
 
@@ -42,22 +42,22 @@ class ClockGUI():
     def __init__(self, combinations): 
         self.combination_counter = 0
         self.angle_width = combinations[self.combination_counter][0]
-        print('Angle width', self.angle_width)
+        print('Angle width:', self.angle_width)
         self.hand_speed = combinations[self.combination_counter][1]
-        print('Hand speed', self.hand_speed)
+        print('Hand speed:', self.hand_speed)
 
         self.rotation_counter = 0
         self.target_trigger = False
         self.num_combinations = len(combinations)
 
-    def updateClock(self, w, nx, ny):  # clock draw function
+    def updateClock(self, w, nx, ny, guiClass):  # clock draw function
         # helper variables 
         x0 = nx/2; lx = 9*nx/20     # center and half-width of clock face
         y0 = ny/2; ly = 9*ny/20
         r2 = 0.8 * min(lx,ly)   # length of hand
 
         # create outline of circle
-        w.create_oval(x0-lx, y0-ly, x0+lx, y0+ly, width=2)  # clock face
+        w.create_oval(x0-lx, y0-ly, x0+lx, y0+ly, width=4)  # clock face
 
         # create the target area
         starting_formula = -(START_ANGLE-90)
@@ -72,14 +72,14 @@ class ClockGUI():
 
         x = x0 + r2 * sin(phi_micros_rad)   # position of end of the arrowhead
         y = y0 - r2 * cos(phi_micros_rad)
-        w.create_line(x0, y0 , x, y, arrow=LAST, width=4)       # draw hand
+        w.create_line(x0, y0 , x, y, arrow=LAST, width=6)       # draw hand
         # print('Angle of hand:', phi_degrees)
         # print('Time:',"%.2f" % t_s_with_micros, 'Angle deg:',"%.2f" %  phi_micros_degrees, 'Angle rad:', "%.2f" % phi_micros_rad)
         #    print("x:", x, ", y:", y)
 
         # know whether you are in the target or not
         if phi_micros_degrees >= START_ANGLE and phi_micros_degrees <= (START_ANGLE+self.angle_width):
-            print("You're in the target!")
+            # print("You're in the target!")
             # write the signal to the ardunio 
             arduino.write(bytes([1]))
             # print(arduino.readline()) # remove print statement when running real exp
@@ -99,26 +99,33 @@ class ClockGUI():
         if self.rotation_counter >= NUM_ROTATIONS:
             # more than X rotations
             
-            # pause screen for a little bit - indicate change 
-            # root.after(5000) # need to pause
 
             # swtich to the next combination
             
             self.combination_counter += 1
             if  self.combination_counter <= self.num_combinations - 1:
                 self.angle_width = combinations[self.combination_counter][0]
-                print('Angle width', self.angle_width)
+                print('Angle width:', self.angle_width)
                 self.hand_speed = combinations[self.combination_counter][1]
-                print('Hand speed', self.hand_speed)
+                print('Hand speed:', self.hand_speed)
 
-                self.rotation_counter = 0   
+                self.rotation_counter = 0  
+
+                # pause screen for a little bit - indicate change 
+                w.delete(ALL)
+                root.after(1000, self.freezeGUI(w, nx, ny, guiClass)) # need to pause
+ 
             else:
                 root.after(1000, root.destroy)
+
+    def freezeGUI(self, w, nx, ny, guiClass):
+        w.delete(ALL)
+        w.after(1500, Clock, w, nx, ny, guiClass) # need to pause
 
 
 def Clock(w, nx, ny, guiClass):                                # clock callback function
     w.delete(ALL)                                              # delete canvas
-    guiClass.updateClock(w, nx, ny)                                        # draw clock
+    guiClass.updateClock(w, nx, ny, guiClass)                                        # draw clock
     w.after(5, Clock, w, nx, ny, guiClass)                  # call callback after 5 ms
 
 # main
@@ -126,7 +133,7 @@ def Clock(w, nx, ny, guiClass):                                # clock callback 
 root = Tk()                                           # create Tk root widget
 root.title("Python clock")
 
-nx = 1000; ny = 1000                                            # canvas size
+nx = 2000; ny = 2000                                            # canvas size
 w = Canvas(root, width=nx, height=ny, bg = "white")         # create canvas w
 w.pack()                                                # make canvas visible
 
